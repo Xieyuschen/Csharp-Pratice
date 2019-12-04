b04d16d3b97dd8b85a8fc737a9dfb94d06816322
Xieyuschen <Xieyuschen@users.noreply.github.com>
2019-12-04 11:53:34 +0800

    12.4

---
+++ Delegate and Event/8.generic delegate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegate_and_Event
+{
+    //在使用泛型委托的时候，委托类和要被委托的类后面要加上<T>表示这是一个泛型类，而包含这两个类的类是不需要包含泛型说明符的
+    public class generic_delegate
+    {
+        delegate void Dosomething<T>(T temp);
+        public class TrafficLight<T>
+        {
+            public T state;
+            public void Show(T t)
+            {
+                Console.WriteLine($"Suprising! This is {t}");
+            }
+        }
+        static void Main()
+        {
+            TrafficLight<int> trafficlight = new TrafficLight<int>();
+            Dosomething<int> dosomething = new Dosomething<int>(trafficlight.Show);
+            dosomething(4);
+            TrafficLight<string> trafficLight1 = new TrafficLight<string>();
+            Dosomething<string> dosomething1 = new Dosomething<string>(trafficLight1.Show);
+            dosomething1("Hello world");
+        }
+    }
+    
+}
---
+++ Delegate and Event/9.generic event.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegate_and_Event
+{
+    class Class1
+    {
+        public delegate void Dosomething<T>(T t);
+        public class Publisher<T>
+        {
+            public event Dosomething<T> Reminder;
+            public void Show(T t)
+            {
+                Reminder(t);
+            }
+        }
+        public class Gut<T>
+        {
+            public void Tag(T t)
+            {
+                Console.WriteLine($"It is morning now and Yuchen has say {t} to you!");
+            }
+        }
+
+        static void Main()
+        {
+            Gut<string> gut = new Gut<string>();
+            Dosomething<string> dosomething = new Dosomething<string>(gut.Tag);
+            Publisher<string> publisher = new Publisher<string>();
+            publisher.Reminder += dosomething;
+            publisher.Show("Wie geht es ihnen?");
+        }
+    }
+}
