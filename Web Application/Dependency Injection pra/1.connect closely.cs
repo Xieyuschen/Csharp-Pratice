3d81e28c4fd9ac4110c43039badf6e52560b3c7b
Xieyuschen <Xieyuschen@users.noreply.github.com>
2020-01-28 22:59:44 +0800

    1.27

---
+++ Web Application/Dependency Injection pra/1.connect closely.cs
@@ -0,0 +1,59 @@
+﻿using System;
+/*
+ this pratice is follow the article provided by the Zhihu,
+ https://www.zhihu.com/question/32108444
+ ja,it's pretty good and lively,even using the language i'm not fimliar to.
+ so i want to rewrite it by the C# to understand it better!
+
+in this part the person have the ipone6 and crab it everytime.
+so what happened if his ipone6 lost and he bought an iphoneX.
+     */
+namespace Dependency_Injection_pra
+{
+    class Program
+    {
+        interface Iiphone
+        {
+            void read(string name);
+            void play(string name);
+        }
+        public class Iphone6 : Iiphone
+        {
+            public void read(string name)
+            {
+                Console.WriteLine($"{name }open the Zhihu and compose a fake story to earn likes");
+            }
+            public void play(string name)
+            {
+                Console.WriteLine($"{name } plays games as a green finger");
+            }
+        }
+
+        public class Person{
+            public Person(string a)
+            {
+                name = a;
+                age = 20;
+                iphone6 = new Iphone6();
+            }
+            public string name { get; set; }
+            private int age { get; set; }
+            private Iphone6 iphone6;
+            public void play()
+            {
+                iphone6.play(name);
+            }
+            public void read()
+            {
+                iphone6.read(name);
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            Person person = new Person("Ming");
+            person.read();
+            person.play();
+        }
+    }
+}
---
+++ Web Application/Dependency Injection pra/2.change iponeX.cs
@@ -0,0 +1,71 @@
+﻿using System;
+/*
+Ming lost his iphone6 and bought the iphoneX,and he still want to do such thing like reading and playing.
+     */
+namespace Dependency_Injection_pra
+{
+    class Program
+    {
+        interface Iiphone
+        {
+            void read(string name);
+            void play(string name);
+        }
+        //public class Iphone6 : Iiphone
+        //{
+        //    public void read(string name)
+        //    {
+        //        Console.WriteLine($"{name} open the Zhihu and compose a fake story to earn likes");
+        //    }
+        //    public void play(string name)
+        //    {
+        //        Console.WriteLine($"{name} plays games as a green finger");
+        //    }
+        //}
+
+        //the first place to change!
+        public class IphoneX : Iiphone
+        {
+            public void read(string name)
+            {
+                Console.WriteLine($"{name} open the Zhihu and compose a fake story to earn likes");
+            }
+            public void play(string name)
+            {
+                Console.WriteLine($"{name} plays games as a green finger");
+            }
+        }
+
+        //the second place to change
+        //there are four iphone6 have to be changed to iphoneX.
+        //this just two method and it's very complex to change,what about more than 10 func happening?
+        public class Person
+        {
+            public Person(string a)
+            {
+                name = a;
+                age = 20;
+                //there are no more Iphone6 but IphoneX
+                iphoneX = new IphoneX();
+            }
+            public string name { get; set; }
+            private int age { get; set; }
+            private IphoneX iphoneX;
+            public void play()
+            {
+                iphoneX.play(name);
+            }
+            public void read()
+            {
+                iphoneX.read(name);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Person person = new Person("Ming");
+            person.read();
+            person.play();
+        }
+    }
+}
